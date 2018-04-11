d393bee6c4fb2bb9f34e6cd320e5ec447596f72c
Mallikharjuna Rao <mallikharjun.kancherla@smactechlabs.com>
2018-04-11 16:12:54 +0530

    before db connection

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 'use strict';
 var express=require('express');
 var app=express();
-var jsonParser=require('body-parser').json;
+var routes=require('./routes');
 
+var jsonParser=require('body-parser').json;
+var logger=require('morgan');
+app.use(logger('dev'));
 app.use(jsonParser());
 
-app.use( (req,res,next)=>{
-    req.body;
-    next();
-    });
+app.use('/questions',routes);
 
-app.use( (req,res,next)=>{
-console.log('first middleware');
-next();
+
+app.use(function(req, res, next){
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
+
+
+
+app.use(function(err, req, res, next){
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	});
+});
+
 var port=process.env.PORT || 3000;
 app.listen(port,()=>{ 
     console.log('express server running on port: 3000\nlocalhost:3000');
